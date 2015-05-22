c9949043e0a184bcf205c74f7d7731962fd48b93
JJ Merelo <jjmerelo@gmail.com>
2015-05-22 23:14:56 +0200

    Adapta a escrutado

--- js/resultados.js
+++ js/resultados.js
@@ -1,5 +1,7 @@
 var resultados;
+var escrutado;
 
 function parse_results( estos_resultados ) {
-    resultados = estos_resultados;
+    resultados = estos_resultados['Resultados'];
+    escrutado = estos_resultados['Escrutado'];
 }
